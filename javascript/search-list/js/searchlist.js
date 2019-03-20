741d92c68911979f7d3abc35fd489e941b2aeadd
Tal <talvinder.singh@sapient.com>
2019-03-20 13:52:29 +0530

    beautified all files

--- javascript/search-list/js/searchlist.js
+++ javascript/search-list/js/searchlist.js
@@ -1,49 +1,48 @@
-var searchlist = (function() {
+var searchlist = (function () {
     var self = this;
     return {
-    init: function(data){
-        var elem = document.getElementById(data.insertIdHere);
-        var listdata = data.listdata;
-        if(elem){
-            elem.innerHTML = `<div class="listparent"><input type="text" id="${data.id}" name="${data.name}" placeholder="${data.placeholder}" class="searchList"></div>`;
-            elem.insertAdjacentHTML('beforeend', '<div class="listcontainer noshow"></div>');
-            var listcontainer = document.getElementsByClassName('listcontainer')[0];
-            listdata.forEach(function(key){
-                listcontainer.insertAdjacentHTML('beforeend',`<div class="listeach">${key.name}</div>`);
-            });
-
-        var inputElem = document.querySelectorAll('.listparent input');
-        var listEachFilter =  document.querySelectorAll('.listcontainer .listeach');   
-
-            inputElem[0].addEventListener('input', function(e){
-                var inputVal = inputElem[0].value.toLowerCase();
-                    listcontainer.classList.remove('noshow');
-                if(inputVal.length < 2){
-                    listEachFilter.forEach(function(key){
-                        key.classList.remove('nomatch');
-                    });
-                    if(inputVal.length == 0){
-                        listcontainer.classList.add('noshow');
-                    }
-                    return false;
-                } 
-
-                listEachFilter.forEach(function(key){
-                    key.classList.remove('nomatch');
-                    if(!(key.innerText).toLowerCase().startsWith(inputVal)){
-                        key.classList.add('nomatch');
-                    }
-
-                    key.addEventListener('click', function(e){
-                        inputElem[0].value = this.innerText;
-                        listcontainer.classList.add('noshow');
-                    })
+        init: function (data) {
+            var elem = document.getElementById(data.insertIdHere);
+            var listdata = data.listdata;
+            if (elem) {
+                elem.innerHTML = `<div class="listparent"><input type="text" id="${data.id}" name="${data.name}" placeholder="${data.placeholder}" class="searchList"></div>`;
+                elem.insertAdjacentHTML('beforeend', '<div class="listcontainer noshow"></div>');
+                var listcontainer = document.getElementsByClassName('listcontainer')[0];
+                listdata.forEach(function (key) {
+                    listcontainer.insertAdjacentHTML('beforeend', `<div class="listeach">${key.name}</div>`);
                 });
 
+                var inputElem = document.querySelectorAll('.listparent input');
+                var listEachFilter = document.querySelectorAll('.listcontainer .listeach');
 
-            });
+                inputElem[0].addEventListener('input', function (e) {
+                    var inputVal = inputElem[0].value.toLowerCase();
+                    listcontainer.classList.remove('noshow');
+                    if (inputVal.length < 2) {
+                        listEachFilter.forEach(function (key) {
+                            key.classList.remove('nomatch');
+                        });
+                        if (inputVal.length == 0) {
+                            listcontainer.classList.add('noshow');
+                        }
+                        return false;
+                    }
+
+                    listEachFilter.forEach(function (key) {
+                        key.classList.remove('nomatch');
+                        if (!(key.innerText).toLowerCase().startsWith(inputVal)) {
+                            key.classList.add('nomatch');
+                        }
+
+                        key.addEventListener('click', function (e) {
+                            inputElem[0].value = this.innerText;
+                            listcontainer.classList.add('noshow');
+                        })
+                    });
+
+
+                });
+            }
         }
     }
-    }
-  })();
-  
+})();
